97d38e0f63776220c9e3086616362698a593b8c1
ALStoilkov <antonistoilkov@gmail.com>
2021-05-11 12:47:38 +0100

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import JumboProfile from "./components/JumboProfile";
+import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
-import React from "react";
+import JumboProfile from "./components/JumboProfile";
+import Sidebar from "./components/Sidebar";
 import fetchUser from "./services/fetchUser";
+import getUsers from "./services/getUsers";
 
 class App extends React.Component {
   state = {
     user: {},
+    listOfUsers: [],
   };
   componentDidMount = async () => {
     const getUser = await fetchUser();
     this.setState({ user: getUser });
+    const listOfUsers = await getUsers();
+    this.setState({ listOfUsers });
   };
   render() {
     return (
@@ -22,7 +27,9 @@ class App extends React.Component {
             <Col md={8}>
               <JumboProfile user={this.state.user} />
             </Col>
-            <Col md={4}></Col>
+            <Col md={4}>
+              <Sidebar listOfUsers={this.state.listOfUsers} />
+            </Col>
           </Row>
         </Container>
       </Router>
