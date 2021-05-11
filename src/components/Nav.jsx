1cb013c8f6290faab2679c204ddd9e24ac8ba46b
Tommy-Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-05-11 21:28:20 +0100

    Nav dropdown user icon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,7 @@ export default class Nav extends Component {
 	state = {
 		search: "",
 	}
+	
 	render() {
 		console.log(process.env.REACT_APP_TOKEN)
 		return (
@@ -109,58 +110,46 @@ export default class Nav extends Component {
 							<br />
 							Notifications
 						</button>
-						<button className=" mebtn dropdown">
+						<button className="  dropdown">
+						<div className="mebtn">
 							<div className="usericon">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 128 128"
-									data-supported-dps="128x128"
-								>
-									<path fill="#e9e5de" d="M0 0h128v128H0z" />
-									<path
-										d="M88.41 84.67a32 32 0 10-48.82 0 66.13 66.13 0 0148.82 0z"
-										fill="#788fa5"
-									/>
-									<path
-										d="M88.41 84.67a32 32 0 01-48.82 0A66.79 66.79 0 000 128h128a66.79 66.79 0 00-39.59-43.33z"
-										fill="#9eb3c8"
-									/>
-									<path
-										d="M64 96a31.93 31.93 0 0024.41-11.33 66.13 66.13 0 00-48.82 0A31.93 31.93 0 0064 96z"
-										fill="#56687a"
-									/>
-								</svg>
+								<img
+									src={this.props.user.image}
+									width="20px"
+									alt="profile"
+								/>
+								<p>Me ▼</p>
 							</div>
-							Me ▼
 							<div class="dropdown-content text-left">
-								<div className="">
-									<img
-										src={this.props.user.image}
-										className="dropdownUserimage"
-										width="50px"
-										alt="profile"
-									/>
-									{this.props.user.username}
-									<br />
-									{this.props.user.bio}<br/>
-									<button>View Profile</button>
-									<hr />
-
-									<h5>Account</h5>
-									<a href="/settings">Settings & Privacy</a>
-									<br />
-									<a href="/help">Help</a>
-									<br />
-									<a href="/languages">Languages</a>
-									<hr />
-									<h5>Manage</h5>
-									<a href="/settings">Posts & Activity</a>
-									<br />
-									<a href="/settings">Job Posting Account</a>
-									<hr />
-									<a href="/settings">Sign Out</a>
+								<img
+									src={this.props.user.image}
+									className="dropdownUserimage"
+									width="50px"
+									alt="profile"
+								/>
+								<div className="dropdownuserinfo">
+									<h6>{this.props.user.username}</h6>
+									{this.props.user.bio}
 								</div>
+								<br />
+								<button className="dropdownbtn" href="/profile">View Profile</button>
+								<hr />
+
+								<h5>Account</h5>
+								<a href="/settings">Settings & Privacy</a>
+								<br />
+								<a href="/help">Help</a>
+								<br />
+								<a href="/languages">Languages</a>
+								<hr />
+								<h5>Manage</h5>
+								<a href="/settings">Posts & Activity</a>
+								<br />
+								<a href="/settings">Job Posting Account</a>
+								<hr />
+								<a href="/settings">Sign Out</a>
 							</div>
+						</div>
 						</button>
 					</div>
 					<div className="navright">
