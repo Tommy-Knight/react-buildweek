f500c1602f30b615e25c845e454041f64e930c33
ALStoilkov <antonistoilkov@gmail.com>
2021-05-11 12:06:25 +0100

    sidebar path fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import JumboProfile from './components/JumboProfile';
-import { Col, Container, Row } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Nav from './components/Nav';
-import React from 'react';
-import fetchUser from './services/fetchUser';
-import Sidebar from './components/Sidebar';
-import getUsers from './services/getUsers';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import JumboProfile from "./components/JumboProfile";
+import Sidebar from "./components/Sidebar";
+import fetchUser from "./services/fetchUser";
+import getUsers from "./services/getUsers";
 
 class App extends React.Component {
   state = {
